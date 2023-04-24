df65ccc54e15c375284dbedcb479b0f509d0b860
MicaelOps <51251504+MicaelOps@users.noreply.github.com>
2023-04-24 08:42:22 +0100

    Add files via upload

--- linux.sh
+++ linux.sh
@@ -1,1 +1,185 @@
-upload once the assignment is over
+#!/bin/bash
+# Author: Micael Cossa
+
+# Boolean values tracker for clarity
+BOOLEAN_TRUE=0
+BOOLEAN_FALSE=1
+
+# Enables file descriptor 3 for reading (Avoid collusion)
+exec 3<> /dev/stdin
+
+readonly BOOLEAN_TRUE
+readonly BOOLEAN_FALSE 
+
+# Checks if Folder exists and it is a folder
+doesFolderExist() {
+    if [ -d "$1" ]; then
+        return $BOOLEAN_TRUE
+    else
+        return $BOOLEAN_FALSE
+    fi
+}
+
+# Checks if file exists and it is not a folder
+doesFileExist() {
+    if [ -f "$1" ]; then
+        return $BOOLEAN_TRUE
+    else
+        return $BOOLEAN_FALSE
+    fi
+}
+
+#asks the user if he wants to overwite
+# Expected Parameters
+# $1 = Destination folder
+checkOverwrite() {
+   
+    read -u 3 -p "Do you wish to overwrite existing contents? (y/anything) " OVERWRITE_STR
+
+    if [ "$OVERWRITE_STR" = "y" ]; then
+        echo "Overwritting..."
+        return $BOOLEAN_TRUE
+    else
+        return $BOOLEAN_FALSE
+    fi
+}
+# Copies all contents of one folder to another
+# Expected paramters :
+# $1 = source folder
+# $2 = destination folder
+# $3 = overrite (BOOLEAN_TRUE or BOOLEAN_FALSE)
+copyContents() {
+    
+    if [ $3 -eq $BOOLEAN_TRUE ]; then
+        cp -r "$1" "$2"
+    else
+        cp -r -n "$1" "$2"
+    fi
+    echo "All files have been written."
+}
+
+# Program starts here
+# Checks if there are no arguments 
+if [ $# -eq 0 ]; then # No arguments provided
+
+    # variable that tracks whether the user inputted correct source and destination folders.
+    CORRECTFOLDERS=$BOOLEAN_FALSE
+
+    SRC=""
+    DEST=""
+    RET2=$BOOLEAN_TRUE
+    
+    # A while loop to persist the user to input correct values
+    while [ $CORRECTFOLDERS -eq $BOOLEAN_FALSE ] 
+    do
+        echo "Please input the source folder "
+        read SRC
+        
+        doesFolderExist $SRC
+        RET=$?
+        
+       
+        if [ $RET -eq $BOOLEAN_FALSE ]; then
+            echo "Invalid source folder"
+            continue
+        fi
+        
+        echo "Please input the destination folder "
+
+        read DEST
+            
+        doesFolderExist "$DEST"
+
+        RET=$?
+
+	    # Check if destination folder exists
+        if [ $RET -eq $BOOLEAN_FALSE ]; then
+            mkdir "$DEST"
+        else
+            # Checks if the user wants to overwrite destination folder.
+            checkOverwrite "$dest"
+            RET2=$?
+        fi
+        
+        CORRECTFOLDERS=$BOOLEAN_TRUE  
+    done
+    
+    copyContents "$SRC/." "$DEST" $RET2
+    
+    unset SRC
+    unset DEST
+    unset RET2
+    unset CORRECTFOLDERS
+
+
+elif [ $# -eq 1 ]; then # There is only one argument
+    
+    doesFileExist "$(readlink -f $1)"
+    
+    RET=$? # Return value
+    
+    if [ $RET -eq $BOOLEAN_TRUE ]; then # Checks whether the file exists and it is a regular file
+    
+        #whereis $1 # Moves to the directory of the file 
+    
+        echo "$1 was found."
+        echo "Reading file...."
+        
+        # Removes variable.
+        unset RET
+
+        # Reads line by line and separates each line with the " " and returns a string array
+        # Expected format: "source dest"
+        while IFS=" " read -r src dest
+        do
+		
+            # checks to see if its a valid source
+            if [ "$src" != "" ]; then
+            
+                # Checks if source folder exists
+                doesFolderExist "$src"
+
+		        RET1=$?
+                
+                # Checks if source folder exists
+                if [ $RET1 -eq $BOOLEAN_FALSE ]; then
+                    echo "Invalid source locattion provided"
+                else
+                    
+                    doesFolderExist "$dest"
+
+                    RET2=$?
+
+                    # Making sure it copies the contents to an existing folder
+                    if [ $RET2 -eq $BOOLEAN_FALSE ]; then
+                        mkdir "$dest"
+                    fi
+                    
+                    # Checks if the user wants to overwrite destination folder.
+                    checkOverwrite "$dest"
+
+                    RET3=$?
+                    
+                    # Copies Contents
+                    copyContents "$src/." "$dest" $RET3                
+                    
+                    
+                    unset RET3
+                fi
+                unset RET1
+            fi 
+        done < "$(readlink -f $1)"  
+
+        echo "Done."
+
+    else
+        echo "Please input the path of a file that exists and is not a folder."
+    fi
+
+# More than one argument provided
+else
+    echo "Too many arguments! Please only input one file"
+fi
+
+
+
--- windows.bat
+++ windows.bat
@@ -1,1 +1,140 @@
-upload once the assignment is over
+:: Author Micael Cossa
+@echo off
+
+set /A BOOLEAN_TRUE=0
+set /A BOOLEAN_FALSE=1
+
+:: Print welcome messages
+echo: 
+echo Welcome to this amazing program!
+echo To quit at any moment press Ctrl + c
+echo:
+
+:: Checks if the first argument does not exist
+if "%~1" == "" (
+
+    :: Asks the user to input source and destination folder to perform a copy
+    call :askFolders source, dest
+    
+    :: To allow the code to run with variable data retrieved from runtime
+    setlocal ENABLEDELAYEDEXPANSION 
+
+    :: Executes the copy of folders
+    call :copyContents !source!, !dest!, !returnvalue!
+
+    endlocal
+
+) else ( 
+
+    :: Checks if second argument does not exist reads
+    if "%~2" == "" (
+
+        :: Reads List file
+        call :readListFile %~1 
+
+    ) else ( 
+        echo Program terminated: Too many arguments! 
+    ) 
+)
+
+:: Closes the program
+EXIT /B 0
+
+ 
+    
+:: Checks if file exists and it is a file
+:: Expected parameters 
+:: %~1 = path to file
+:: %~2 = return value
+:doesFileExist
+    if exist %~1 ( set %~2=%BOOLEAN_TRUE% ) else ( set %~2=%BOOLEAN_FALSE% )
+EXIT /B 0
+
+
+:: Checks if Folder exists and it is a folder
+:: Expected parameters 
+:: %~1 = path to folder
+:: %~2 = return variable
+:doesFolderExist
+    if exist %~1\ ( set %~2=%BOOLEAN_TRUE% ) else ( set %~2=%BOOLEAN_FALSE% )
+EXIT /B 0
+
+
+:: Asks the user while persisting for correct values
+:: to input source and destination folders location
+:askFolders
+
+    :: Asks the user to input source folder
+    set /p SRC= "Please input the source folder "
+
+    :: Checks if source folder exist
+    call :doesFolderExist %SRC%, returnvalue
+
+    :: If does not exist repeats the function
+    if %returnvalue% EQU %BOOLEAN_FALSE% (
+            echo The folder does not exist
+            goto askFolders
+    )
+
+    set %~1=%SRC%
+
+    :: Asks the destination folder
+    set /p DEST="Please input the destination folder "
+    set %~2=%DEST%
+
+EXIT /B 0
+
+:: Reads list file and copies the sources to the destination folders
+:: Expected format in every line: '<source> <destination>'
+:: Expected Parameter:
+:: %~1 = path to list file
+:readListFile
+    :: To allow the code to run with variable data retrieved from runtime inside the for loop
+    setlocal ENABLEDELAYEDEXPANSION
+
+    :: This enable to track the loop iterations
+    set /A counter = 0
+
+    :: Reads line by line the file in the first parameter
+    :: Separates using space character and gets the first and second results after split
+    for /f "tokens=1,2 delims= " %%A in (%~1) do (
+            
+        :: Checks if the source and destination are not the same or not empty
+        if not "%%A%%B" == "%%B%%A" (              
+
+            set /A counter = 1
+                
+            :: Checks if source folder exists
+            call :doesFolderExist %%A, returnvalue
+                
+            :: Get the updated value from doesFolderExist returnvalue
+            set /A updatedreturn = !returnvalue!
+                
+            if !updatedreturn! EQU %BOOLEAN_TRUE% (
+
+                echo Copying %%A to %%B
+                call :copyContents %%A %%B
+
+            ) else ( echo Source %%A does not exist )
+                
+        ) else ( echo Invalid file line detected )
+    )
+
+    :: the for loop did not have a single successful iteration
+    if !counter! EQU 0 echo If you are seeing this message it means list file does not exist or is corrupted
+        
+    endlocal
+
+EXIT /B 0
+
+:: Copies all contents of one folder to another using xcopy
+:: /F = Displays full source and destination file names while copying.
+:: /E = Copies directories and subdirectories, including empty ones.
+:: /I = If the destination does not exist and copies more than one file, it assumes that the destination must be a directory.
+:: xcopy has built in function to ask for overwrite.
+:: Expected paramters :
+:: %~1 = source folder
+:: %~2 = destination folder
+:copyContents
+    xcopy %~1 %~2 /F /E /I
+EXIT /B 0
